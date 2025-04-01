9f361516b09962032521fd4c19cff088471290d6
那就叫小徐吧 <x17605049954@163.com>
2025-04-01 17:16:41 +0800

    Initial commit

---
+++ src/main/java/com/xiaoxu/shuati/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.xiaoxu.shuati.mapper;
+
+import com.xiaoxu.shuati.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/xiaoxu/shuati/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,30 @@
+package com.xiaoxu.shuati.model.dto.postfavour;
+
+import com.xiaoxu.shuati.common.PageRequest;
+import com.xiaoxu.shuati.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ *
+
+
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
